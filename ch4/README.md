8cb9d5c9732d83b777d1cbc51e8073ccb7f513d5
Azderica <mh97888@gmail.com>
2021-03-20 14:36:24 +0900

    linit local db

--- ch4/README.md
+++ ch4/README.md
@@ -1,1 +1,19 @@
 # ch4. Todo List 만들기
+
+- [원문 코드](https://github.com/HeropCode/Vue-Todo-app)
+
+## 컴포넌트 구성
+
+![image](https://user-images.githubusercontent.com/42582516/111631388-ad2dbf00-8836-11eb-92a6-db14089adb44.png)
+
+## DB관련
+
+### Local Storage
+
+- [MDN DOC](https://developer.mozilla.org/ko/docs/Web/API/Window/localStorage)
+
+### Low db
+
+`npm install lodash lowdb`
+
+lodash 기반이므로 lodash를 설치해야한다.
--- vue-todo-app/components/TodoApp.vue
+++ vue-todo-app/components/TodoApp.vue
@@ -6,16 +6,39 @@
 </template>
 
 <script>
-import TodoCreator from "./TodoCreator.vue";
-import TodoItem from "./TodoItem.vue";
+import lowdb from 'lowdb'
+import LocalStorage from 'lowdb/adapters/LocalStorage'
+import TodoCreator from './TodoCreator.vue'
+import TodoItem from './TodoItem.vue'
 
 export default {
   components: {
     TodoCreator,
     TodoItem,
   },
-};
+  data() {
+    return {
+      db: null,
+    }
+  },
+  created() {
+    this.initDB()
+  },
+  methods: {
+    initDB() {
+      // db name
+      const adapter = new LocalStorage('todo-app')
+      this.db = lowdb(adapter)
+
+      // Local DB 초기화
+      this.db
+        .defaults({
+          todos: [], // Collection
+        })
+        .write()
+    },
+  },
+}
 </script>
 
-<style>
-</style>
+<style></style>
